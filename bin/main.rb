8f6870261d9ab516deeca6937ae4c8647c26d414
abdelp <juniorperezpy@gmail.com>
2020-03-09 18:18:31 -0300

    Update game flow to select 9 slots and turning users

--- bin/main.rb
+++ bin/main.rb
@@ -11,9 +11,7 @@ class Board
 
   def select_slot(player_number, slot_number)
     x, y = get_coordinates(slot_number)
-    if self.slots[y][x].is_a?(Integer)
-      self.slots[y][x] = player_number == 1 ? "X" : "O"
-    end
+    slots[y][x] = player_number == 1 ? 'X' : 'O' if slots[y][x].is_a?(Integer)
   end
 
   def get_coordinates(slot_number)
@@ -21,20 +19,14 @@ class Board
     y = (slot_number.to_f / 3).ceil - 1
     [x, y]
   end
-
 end
 
+# Class for players
 class Player
-  @@players_count = 0
   attr_reader :player_number
 
-  def initialize
-    increment_players
-    self.player_number = @@players_count
-  end
-
-  def increment_players
-    @@players_count += 1
+  def initialize(player_number)
+    self.player_number = player_number
   end
 
   private
@@ -52,24 +44,27 @@ option = gets.chomp
 
 if option == 'play'
   board = Board.new
-  player1 = Player.new
-  player2 = Player.new
+  player1 = Player.new(1)
+  player2 = Player.new(2)
+  current_player = player1
+  slots_selected = 0
 
   p board.slots[0], board.slots[1], board.slots[2]
 
   puts 'Select a slot:'
 
-  p "Player #{player1.player_number}"
-  slot_selected = gets.chomp.to_i
-  board.select_slot(player1.player_number, slot_selected)
-  p board.slots[0], board.slots[1], board.slots[2]
-  p 'Now, your move is displayed on the board'
+  while slots_selected < 9
+    p "Player #{current_player.player_number}"
+    slot_selected = gets.chomp.to_i
+    board.select_slot(current_player.player_number, slot_selected)
+    p board.slots[0], board.slots[1], board.slots[2]
+    p 'Now, your move is displayed on the board'
 
-  p "Player #{player2.player_number}"
-  slot_selected = gets.chomp.to_i
-  board.select_slot(player2.player_number, slot_selected)
-  p board.slots[0], board.slots[1], board.slots[2]
-  p 'Now, your move is displayed on the board'
+    slots_selected += 1
+    current_player = current_player.player_number == 1 ? player2 : player1
+  end
+
+  p "Game finished"
 else
   exit
 end
